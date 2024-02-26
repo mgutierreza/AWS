0acbc4b050f7406c4b7eec0b7362cb1a66d591b3
Manuel Gutierrez <manuel.gutierrez.aguirre@outlook.com>
2024-02-25 19:01:00 -0500

    Practica IF y Locals
    Practica IF y Locals

---
+++ Terraform/Learn/Practicas/EC2_Locals/locals.tf
@@ -0,0 +1,3 @@
+locals {
+    sufix = "${var.tags.Project}-${var.tags.Ambiente}-${var.tags.Region}"
+}
---
+++ Terraform/Learn/Practicas/EC2_Locals/variables.tf
@@ -0,0 +1,36 @@
+variable "Virginia_cidr" {
+  description = "CIDR Virginia"
+  type        = string
+  sensitive   = false
+}
+
+variable "tags" {
+  description = "Tags generales de los servicios"
+  type        = map(string)
+}
+
+variable "public_subnet" {
+  description = "CIDR Public Subnet"
+  type        = string
+}
+
+variable "private_subnet" {
+  description = "CIDR Private Subnet"
+  type        = string
+}
+
+variable "ec2_parametros" {
+  description = "Parémetros de la instancia"
+  type        = map(string)
+}
+
+variable "instancias" {
+  description = "Nombres de la instancia"
+  type        = map(string)
+
+}
+
+variable "enable_monitoring" {
+  description = "Habilita el despliegue de un servidor de monitoreo"
+  type        = bool
+}
